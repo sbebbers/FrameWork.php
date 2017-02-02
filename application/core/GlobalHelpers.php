3f2e9da9f4f13dfaa09047cc4a8195a8fc9c1593
sbebbers <sbebbington@reapit.com>
2017-02-02 11:11:30 +0000

    Minor refactor.

--- application/core/GlobalHelpers.php
+++ application/core/GlobalHelpers.php
@@ -83,7 +83,6 @@ function setTimeZone(string $timeZone){
 	date_default_timezone_set($timeZone);
 }
 
-
 /**
  * Gets user IP address
  *
@@ -97,8 +96,7 @@ function setTimeZone(string $timeZone){
 function getUserIPAddress(){
 	$client		= $_SERVER['HTTP_CLIENT_IP'] ?? '';
 	$forward	= $_SERVER['HTTP_X_FORWARDED_FOR'] ?? '';
-	$remote		= $_SERVER['REMOTE_ADDR'] ?? '';
-	$ip			= $remote;
+	$ip			= $_SERVER['REMOTE_ADDR'] ?? '';
 	if(filter_var($client, FILTER_VALIDATE_IP)){
 		$ip		= $client;
 	}else if(filter_var($forward, FILTER_VALIDATE_IP)){
--- public_html/Index.php
+++ public_html/Index.php
@@ -1,61 +1,59 @@
 <?php
-	use Application\Core\Framework\Core;
-	
-	require_once(serverPath('/core/FrameworkCore.php'));
-	require_once(serverPath('/core/GlobalHelpers.php'));
+use Application\Core\Framework\Core;
 
-	header('X-Content-Type-Options: nosniff');
-	header('X-XSS-Protection: 1');
-	header('Strict-Transport-Security: max-age=31536000; includeSubDomains');
-	session_set_cookie_params(0, '/', getDomain(host(), ($https = isHttps()) ), $https, true);
-	if(session_id() == ""){
-		session_start();
-	}
+require_once(serverPath('/core/FrameworkCore.php'));
+require_once(serverPath('/core/GlobalHelpers.php'));
+
+header('X-Content-Type-Options: nosniff');
+header('X-XSS-Protection: 1');
+header('Strict-Transport-Security: max-age=31536000; includeSubDomains');
+session_set_cookie_params(0, '/', getDomain(host(), ($https = isHttps()) ), $https, true);
+if(session_id() == ""){
+	session_start();
+}
 	
-	class Index
-	{
-		protected $core;
-		public $timeZone = "Europe/London";
-		
-		/**
-		 * This will initiate the core to load the view
-		 * according to the uri path, one may also
-		 * change the default timezone for the project
-		 * by altering the public $timeZone string above
-		 * for a list of valid timezones, see:
-		 * http://php.net/manual/en/timezones.php
-		 * 
-		 * @param	na
-		 * @author	sbebbington
-		 * @date	24 Jan 2017 - 09:49:15
-		 * @version	0.0.2
-		 * @return	
-		 * @todo
-		 */
-		public function __construct(){
-			$this->core	= new \Application\Core\Framework\Core();
-			setTimeZone($this->timeZone);
-			$this->core->loadPage();
-		}
-	}
-	
+class Index
+{
+	protected $core;
+	public $timeZone = "Europe/London";
+
 	/**
-	 * Determines server path and will include additional route with optional parameter
-	 *
-	 * @param	string
-	 * @author	Shaun
-	 * @date	9 Jun 2016 10:04:26
-	 * @version	0.0.1
-	 * @return	string
+	 * This will initiate the core to load the view
+	 * according to the uri path, one may also
+	 * change the default timezone for the project
+	 * by altering the public $timeZone string above
+	 * for a list of valid timezones, see:
+	 * http://php.net/manual/en/timezones.php
+	 * 
+	 * @param	na
+	 * @author	sbebbington
+	 * @date	24 Jan 2017 - 09:49:15
+	 * @version	0.0.2
+	 * @return	
 	 * @todo
 	 */
-	function serverPath($routeTo = null){
-		$_x = str_replace("\\", "/", dirname(__FILE__)) . '/application';
-		$_x = str_replace("public_html/", "", $_x);
-		$_x .= ($routeTo === null) ? '' : $routeTo;
-		return str_replace("//", "/", $_x);
+	public function __construct(){
+		$this->core	= new \Application\Core\Framework\Core();
+		setTimeZone($this->timeZone);
+		$this->core->loadPage();
 	}
-?>
-<?php
-	// Creates new instance and therefore initiates the controllers, models and views etc...
-	$page = new Index();
+}
+
+/**
+ * Determines server path and will include additional route with optional parameter
+ *
+ * @param	string
+ * @author	Shaun
+ * @date	9 Jun 2016 10:04:26
+ * @version	0.0.1
+ * @return	string
+ * @todo
+ */
+function serverPath($routeTo = null){
+	$_x = str_replace("\\", "/", dirname(__FILE__)) . '/application';
+	$_x = str_replace("public_html/", "", $_x);
+	$_x .= ($routeTo === null) ? '' : $routeTo;
+	return str_replace("//", "/", $_x);
+}
+// Creates new instance and therefore initiates the controllers, models and views etc...
+$page = new Index();
