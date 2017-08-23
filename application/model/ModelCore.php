289be9d0a28dc1fa07495b170b11ff174e6c2fe7
sbebbers <sbebbington@reapit.com>
2017-08-23 10:51:29 +0100

    Typo removed in the model core

--- application/model/ModelCore.php
+++ application/model/ModelCore.php
@@ -18,7 +18,7 @@ class ModelCore
 		$this->lib		= new \Application\Library\Library();
 		
 		try{
-			$this->connection = new \PDO("mysql:host={$dbConfig['host']};port:{$dbConfig['port']};dbname={$this->db};charset={$this->charSet}", $dbConfig['user'], $password);
+			$this->connection = new \PDO("mysql:host={$dbConfig['host']};port={$dbConfig['port']};dbname={$this->db};charset={$this->charSet}", $dbConfig['user'], $password);
 		}catch(PDOException $e){
 			echo '<pre>There was an issue connecting to the database</pre>';
 			$this->lib->debug($e,true);
